b5130d9f5dcf096e70ad28424dca24b8000196ab
Simon Wahlström <simon.otdw@gmail.com>
2018-05-17 11:21:10 +0200

    feat: CLI calc command BMR calculation

--- cmd/macros/internal/cli/commands/calc/calc.go
+++ cmd/macros/internal/cli/commands/calc/calc.go
@@ -2,13 +2,17 @@ package calc
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/reblex/macros/cmd/macros/internal/cli/base"
+	"github.com/reblex/macros/cmd/macros/internal/profile"
+	"github.com/reblex/macros/pkg/bmr"
 )
 
 var CmdCalc = &base.Command{
 	Name:        "calc",
-	Usage:       "calc [flag]... calculation",
+	Usage:       "calc [flag]... calculation <weight>",
 	Description: "Calculate Macros.",
 	Help: `Overview:
     Calculates meal and energy related values based on profile settings.
@@ -22,6 +26,7 @@ Calculations:
 Flags:
     -h, -help			Print this help message.
     `,
+	CustomFlags: true,
 }
 
 var (
@@ -36,8 +41,34 @@ func init() {
 }
 
 func run(cmd *base.Command, args []string) {
-	if calcH {
+	if calcH || len(args) < 1 {
 		fmt.Println(CmdCalc.Help)
 		return
 	}
+
+	var p profile.Profile
+	p.Name = "oscar"
+	p.Load("config/profiles.json")
+
+	fmt.Println(p.Data)
+
+	switch args[0] {
+	case "bmr":
+		if len(args) < 2 {
+			fmt.Println("BMR needs weight argument!\n macros calc bmr <weight>")
+			os.Exit(1)
+		}
+
+		weight, _ := strconv.ParseFloat(args[1], 64)
+
+		bmr, err := bmr.Calculate(p.Data.Gender, p.Data.Standard, weight, p.Data.Height, p.Data.Age)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("BMR: ", bmr)
+	}
+
 }
